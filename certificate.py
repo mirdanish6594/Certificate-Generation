22b026a3428ad88e6e84e78bc9c306ea0b35a832
Hash-D25 <149588109+Hash-D25@users.noreply.github.com>
2024-08-16 14:30:36 +0530

    increased the font-size ; making the certificate images save in a single pdf

--- certificate.py
+++ certificate.py
@@ -4,9 +4,8 @@ import csv
 
 template_path = "Demo_Data/demo_template.png"
 output_directory = "output"
-font_path = "fonts/CormorantGaramond-Italic.ttf"
-
-csv_file_path="Demo_Data/demo_data.csv"
+fontGaramondIt = "fonts/CormorantGaramond-Italic.ttf"
+csv_file_path = "Demo_Data/demo_data.csv"
 
 def fetch_data_from_csv(csv_file_path):
     data = []
@@ -16,28 +15,31 @@ def fetch_data_from_csv(csv_file_path):
             data.append(dict(row))
     return data
 
-users=fetch_data_from_csv(csv_file_path)
+users = fetch_data_from_csv(csv_file_path)
 
 def generate_certificates(users, template_path, output_directory, font_path):
-    font = ImageFont.truetype(font_path, 48)
+    font = ImageFont.truetype(font_path, 70)
     template = Image.open(template_path)
+    certificate_images = []
+
     for user in users:
         certificate = template.copy()
         draw = ImageDraw.Draw(certificate)
-        draw.text((920,766), user["Name"], (0, 0, 0), font=font)
-        certificate.save(f"{output_directory}/{user["Name"]}_certificate.png")
+        draw.text((800, 738), user["Name"], (0, 0, 0), font=font)
+        certificate_images.append(certificate)
 
-generate_certificates(users, template_path,output_directory,font_path)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    
+    pdf_path = os.path.join(output_directory, "certificates.pdf")
+    
+    # Save as pdf
+    certificate_images[0].save(
+        pdf_path, "PDF", resolution=100.0, save_all=True, append_images=certificate_images[1:]
+    )
+    print(f"PDF saved at {pdf_path}")
 
-DeleteNames=[]  #Add the names to be deleted in this list
-def delete_certificates(DeleteNames,template_path,output_directory):
-    for name in DeleteNames:
-        file_path = f"{output_directory}/{name}_certificate.png"
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            print(f"File {name}_certificate.png has been deleted.")
-        else:
-            print(f"The file {name}_certificate.png does not exist.")
+# Generate the certificates and save them as a PDF
+generate_certificates(users, template_path, output_directory, fontGaramondIt)
 
-# delete_certificates(DeleteNames,template_path,output_directory) 
 
